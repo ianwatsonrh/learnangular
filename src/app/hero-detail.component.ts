7c65cbc9c8ee5e41c2f1bb1e021b01d287447b7a
Ian Watson <ianwatson@redhat.com>
2017-02-21 15:02:38 +0000

    completed tutorial

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -9,7 +9,8 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: 'hero-detail.component.html'
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 	@Input()
@@ -29,5 +30,10 @@ export class HeroDetailComponent implements OnInit {
 
 	goBack(): void {
 	  this.location.back();
+	}
+
+	save(): void {
+	  this.heroService.update(this.hero)
+	    .then(() => this.goBack());
 	}
 }
